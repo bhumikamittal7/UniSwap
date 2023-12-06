4de47a530abd2a17aef31b1aa9861e4e8016108c
bhumikamittal7 <bhumika.mittal_ug24@ashoka.edu.in>
2023-12-06 16:24:36 +0530

    frontend update

--- AshokuCoinMain/token.sol
+++ AshokuCoinMain/token.sol
@@ -16,9 +16,3 @@ contract AshokuCoin is ERC20, AccessControl {
         _mint(to, amount);
     }
 }
-
-/*
-I want to build a game where player starts by connecting metamask wallet and then play a quiz game to answer 5 questions. 
-If all 5 questions are right, the user gets 0.5 ACS which will be minted using the token deplayment address and deposited to the player's metamask account. 
-*/
-
